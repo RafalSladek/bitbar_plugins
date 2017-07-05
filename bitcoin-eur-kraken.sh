6ced70b3f6e585e4eaed4d51b60258ffd7227b9d
Rafal Sladek <rafal.sladek@gmail.com>
2017-07-05 17:58:06 +0200

    added bitcoin

---
+++ bitcoin-eur-kraken.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# <bitbar.title>Kraken.com XBTEUR last price</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>R.S.</bitbar.author>
+# <bitbar.author.github>rafalsladek</bitbar.author.github>
+# <bitbar.desc>Gives the last price of bitcoin to euro from Kraken.com</bitbar.desc>
+# <bitbar.image>https://i.imgur.com/iGX2yjR.png</bitbar.image>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+
+# <pair_name> = pair name
+# a = ask array(<price>, <whole lot volume>, <lot volume>),
+# b = bid array(<price>, <whole lot volume>, <lot volume>),
+# c = last trade closed array(<price>, <lot volume>),
+# v = volume array(<today>, <last 24 hours>),
+# p = volume weighted average price array(<today>, <last 24 hours>),
+# t = number of trades array(<today>, <last 24 hours>),
+# l = low array(<today>, <last 24 hours>),
+# h = high array(<today>, <last 24 hours>),
+# o = today's opening price
+
+bitcoin=$(curl -s "https://api.kraken.com/0/public/Ticker?pair=XBTEUR" | tr -d '{}"[]' | tr ':,' '\n' | grep -A1 "^c$" | tail -1)
+
+echo "$(printf "₿ %.3f \n" "$bitcoin") | size=13"
+echo "---"
+echo "Kraken.com | href=\"https://www.kraken.com/\""
