5b49645e3053ab2fd336dce07ccbf94a2729bcae
Rafal Sladek <RafalSladek@users.noreply.github.com>
2017-07-05 09:59:08 +0200

    added ether-eur plugin

---
+++ ether-eur-kraken.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# <bitbar.title>Kraken.com ETHEUR last price</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>G.G.</bitbar.author>
+# <bitbar.author.github>ggrelet</bitbar.author.github>
+# <bitbar.desc>Gives the last price of ether to euro from Kraken.com</bitbar.desc>
+# <bitbar.image>https://i.imgur.com/iGX2yjR.png</bitbar.image>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+
+# <pair_name> = pair name
+# a = ask array(<price>, <whole lot volume>, <lot volume>),
+# b = bid array(<price>, <whole lot volume>, <lot volume>),
+# c = last trade closed array(<price>, <lot volume>),
+# v = volume array(<today>, <last 24 hours>),
+# p = volume weighted average price array(<today>, <last 24 hours>),
+# t = number of trades array(<today>, <last 24 hours>),
+# l = low array(<today>, <last 24 hours>),
+# h = high array(<today>, <last 24 hours>),
+# o = today's opening price
+
+val=$(curl -s "https://api.kraken.com/0/public/Ticker?pair=ETHEUR" | tr -d '{}"[]' | tr ':,' '\n' | grep -A1 "^c$" | tail -1)
+
+echo "$(printf "𝚵 %.3f \n" "$val") | size=13"
+echo "---"
+echo "Kraken.com | href=\"https://www.kraken.com/\""
