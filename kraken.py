cbbfefd56a73df3b003cf9b391237116d7bbbd9c
Rafal <rafal.sladek@gmail.com>
2017-09-09 13:59:17 +0200

    replaced shell with python

--- kraken.py
+++ kraken.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # coding: utf8
 import requests
 import sys
@@ -5,6 +6,17 @@ import json
 import locale
 locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
 
+
+# <pair_name> = pair name
+# a = ask array(<price>, <whole lot volume>, <lot volume>),
+# b = bid array(<price>, <whole lot volume>, <lot volume>),
+# c = last trade closed array(<price>, <lot volume>),
+# v = volume array(<today>, <last 24 hours>),
+# p = volume weighted average price array(<today>, <last 24 hours>),
+# t = number of trades array(<today>, <last 24 hours>),
+# l = low array(<today>, <last 24 hours>),
+# h = high array(<today>, <last 24 hours>),
+# o = today's opening price
 
 def getUrl(x, z):
     return str("https://api.kraken.com/0/public/Ticker?pair=" + makePair(x, z))
@@ -19,34 +31,39 @@ def makePair(x, z):
 
 
 def getResponse(x, z):
-    try:
-        current_url = getUrl(x, z)
-        # print currentUrl
-        data = requests.get(current_url).json()
-        # print data
-        symbol = makeResultPair(x, z)
-        # print symbol
-        price_str = data['result']['%s' % symbol]['c'][0]
-        return price_str
-    except:
-        # print "Unexpected error:", sys.exc_info()[0]
-        pass   
+    current_url = getUrl(x, z)
+    # print currentUrl
+    data = requests.get(current_url).json()
+    # print data
+    symbol = makeResultPair(x, z)
+    # print symbol
+    price_str = data['result']['%s' % symbol]['c'][0]
+    return price_str
+
 
 def formatPrice(unformatted_price, coin_symbol):
-    price = locale.currency(float(unformatted_price), symbol=False)
+    price = locale.currency(float(unformatted_price),
+                            symbol=False, grouping=True)
     return str(coin_symbol + " " + price + " €")
+
+
+def formatToBitBar(output):
+    return str(output + "\n")
 
 
 def getAllPrices(all_pairs):
     for (crypto_currency, fiat_currency, coin_symbol) in all_pairs:
         try:
             price_str = getResponse(crypto_currency, fiat_currency)
-            print formatPrice(price_str, coin_symbol)
-        except: 
+            print formatToBitBar(formatPrice(price_str, coin_symbol))
+        except:
             pass
 
+
 def run():
-    list_of_tuples = [['ETH', 'EUR', '𝚵'], ['XMR', 'EUR', 'Ⓜ'], ['ZEC', 'EUR', 'ⓩ'], ['LTC', 'EUR', 'Ł'], ['BCH', 'EUR', '฿'], ['XBT', 'EUR', '₿']]
+    list_of_tuples = [['ETH', 'EUR', '𝚵'], ['XMR', 'EUR', 'Ⓜ'], ['ZEC', 'EUR', 'ⓩ'], [
+        'LTC', 'EUR', 'Ł'], ['BCH', 'EUR', '฿'], ['XBT', 'EUR', '₿']]
     getAllPrices(list_of_tuples)
+
 
 run()
